c56669291b4e978d2818da64cb15c148ad1a33e7
Alexandre Cadima Leal <alexandre.cadima@gmail.com>
2025-10-07 23:39:25 -0300

    [Sprint 1][Task 6] test: add more routes tests
    add routes defined in api_schema.yml

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,6 +1,9 @@
 use axum::serve::Serve;
 use axum::Router;
 use std::error::Error;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::post;
 use tower_http::services::ServeDir;
 
 // This struct encapsulates our application-related logic.
@@ -16,7 +19,14 @@ impl Application {
         // Move the Router definition from `main.rs` to here.
         // Also, remove the `hello` route.
         // We don't need it at this point!
-        let router = Router::new().nest_service("/", ServeDir::new("assets"));
+        let router = Router::new()
+            .nest_service("/", ServeDir::new("assets"))
+            .route("/signup", post(signup))
+            .route("/login", post(login))
+            .route("/logout", post(logout))
+            .route("/verify-2fa", post(verify_2fa))
+            .route("/verify-token", post(verify_token));
+
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         println!("listening on {}", listener.local_addr().unwrap());
@@ -32,4 +42,26 @@ impl Application {
         println!("listening on {}", &self.address);
         self.server.await
     }
+}
+
+// Example route handler.
+// For now we will simply return a 200 (OK) status code.
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
 }
