4ce4c156431e737459b6b2281a0bcba558d29c5d
Alexandre Cadima Leal <alexandre.cadima@gmail.com>
2025-10-13 17:56:48 -0300

    [Sprint 2][Task 2] feat: implement signup route error cases
    introduce `AuthAPIError` for user validation errors, map errors to HTTP responses, and add corresponding test cases.

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,5 +1,5 @@
-use crate::app_state::AppState;
-use crate::domain::user::User;
+use crate::{app_state::AppState,
+            domain::{error::AuthAPIError, user::User}};
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 
@@ -7,23 +7,36 @@ pub async fn signup(
     // TODO: Use Axum's state extractor to pass in AppState
     State(state): State<AppState>,
     Json(request): Json<SignupRequest>,
-) -> impl IntoResponse {
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let email = request.email;
+    let password = request.password;
+
+    // TODO: early return AuthAPIError::InvalidCredentials if:
+    // - email is empty or does not contain '@'
+    // - password is less than 8 characters
+    if email.is_empty() || !email.contains('@') || password.len() < 8 {
+        return Err(AuthAPIError::InvalidCredentials);
+    }
     // Create a new `User` instance using data in the `request`
-    let user = User::new(request.email, request.password, request.requires_2fa);
+    let user = User::new(email, password, request.requires_2fa);
 
     let mut user_store = state.user_store.write().await;
 
+    // TODO: early return AuthAPIError::UserAlreadyExists if email exists in user_store.
+    if user_store.get_user(user.email.as_ref()).is_ok() {
+        return Err(AuthAPIError::UserAlreadyExists);
+    }
+
     // TODO: Add `user` to the `user_store`. Simply unwrap the returned `Result` enum type for now.
-    user_store.add_user(user).unwrap();
+    // TODO: instead of using unwrap, early return AuthAPIError::UnexpectedError if add_user() fails.
+    user_store.add_user(user).map_err(|_| AuthAPIError::UnexpectedError)?;
 
     let response = Json(SignupResponse {
         message: "User created successfully!".to_string(),
     });
 
-    (StatusCode::CREATED, response)
+    Ok((StatusCode::CREATED, response))
 }
-
-//...
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct SignupResponse {
