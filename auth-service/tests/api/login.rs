b0c039e2e8f9ccb805d22818fdff327aa8d737bc
Alexandre Cadima Leal <alexandre.cadima@gmail.com>
2026-04-14 00:33:00 -0300

    feat: Update email mocking path in login tests and reformat Cargo.lock files
    standardize email mocking path in `login` API tests from `/email` to `/emails` and re-align the corresponding assertion. Resolve formatting inconsistencies in affected `Cargo.lock` files for dependencies and checksum values.

--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -22,14 +22,6 @@ async fn should_return_200_if_valid_credentials_and_2fa_disabled() {
 
     assert_eq!(response.status().as_u16(), 201);
 
-
-    Mock::given(path("/email"))
-        .and(method("POST"))
-        .respond_with(ResponseTemplate::new(200))
-        .expect(1)
-        .mount(&app.email_server)
-        .await;
-
     let login_body = serde_json::json!({
         "email": random_email,
         "password": "password123",
@@ -61,6 +53,13 @@ async fn should_return_206_if_valid_credentials_and_2fa_enabled() {
     let response = app.post_signup(&signup_body).await;
 
     assert_eq!(response.status().as_u16(), 201);
+
+    Mock::given(path("/emails"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
 
     let login_body = serde_json::json!({
         "email": random_email,
